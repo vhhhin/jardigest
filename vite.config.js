dfe04d99e01c83cabfdf3de3e05eeb1204f0eb85
zineb715 <152994780+vhhhin@users.noreply.github.com>
2025-06-20 17:59:52 +0100

    Fix Vercel 404 issue with base config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Configuration Vite en JSX
+// ✅ Configuration corrigée pour le déploiement
 export default defineConfig({
   plugins: [react()],
+  base: './', // 
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
